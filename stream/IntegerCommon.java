307b4cd6d960391df12734401c0b1194bb3a4986
javajb99 <45365212+javajb99@users.noreply.github.com>
2018-12-03 19:30:58 +0200

    stream

---
+++ stream/IntegerCommon.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class IntegerCommon {
+
+	public static boolean get2Int(Integer i1)
+	{
+		///....
+		///.....
+		// .................................
+		return i1 > 0;
+	}
+	
+}
