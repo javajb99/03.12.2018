c81065a17db786f543421e39793bd846c6f95afb
javajb99 <45365212+javajb99@users.noreply.github.com>
2018-12-03 19:29:51 +0200

    Stream java 8

---
+++ ParallelStreams.java
@@ -0,0 +1,18 @@
+package stream;
+
+public class ParallelStreams {
+
+	/*
+	 * A parallel stream has a much higher overhead compared to a sequential one. 
+	 * Coordinating the threads takes a significant amount of time. 
+	 * I would use sequential streams by default and only consider parallel ones if
+	 * I have a massive amount of items to process 
+	 * (or the processing of each item takes time and is parallelizable)
+	 * I have a performance problem in the first place
+	 * I don't already run the process in a multi-thread environment 
+	 * (for example: in a web container, if I already have many requests to process in parallel, 
+	 * adding an additional layer of parallelism inside each request could have more 
+	 * negative than positive effects)
+	 *  Only a measurement will tell you if the parallelism is worth it or not.
+	 */
+}
